4f5338db1d7a2600d7907fdbdef9dca0f153551f
Ali Ebrahiminejad <a.ebrahiminejad2@gmail.com>
2016-12-20 01:26:32 +0330

    Add java codes to project.

---
+++ JavaCodes/src/gui/Initializer.java
@@ -0,0 +1,22 @@
+package gui;
+
+/**
+ * Ali EbrahimiNejad
+ * Created by apadana on 12/20/16.
+ */
+public class Initializer {
+
+    private Initializer() {
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        new Initializer();
+
+    }
+
+}
