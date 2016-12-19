9ecd781bb6338165a96520ef256f787d9ff50647
Ali Ebrahiminejad <a.ebrahiminejad2@gmail.com>
2016-12-20 01:59:33 +0330

    Add a frame by intellij gui form (Swing).

---
+++ JavaCodes/src/gui/MainForm.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by apadana on 12/20/16.
+ */
+public class MainForm {
+    private JPanel panel1;
+    private JButton exitButton;
+    private JButton startButton;
+}
